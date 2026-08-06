6823523a6c3ff238ccd3724b409c3180f4e697a2
Rodrigo <60452545+rodrigolitzius@users.noreply.github.com>
2026-08-06 14:22:03 -0300

    Consertado bimestres repetidos

--- src/mod/bimestres.py
+++ src/mod/bimestres.py
@@ -2,12 +2,15 @@ from util import Data
 
 def print_bimestres(data, escola):
     print(escola["NomeEscola"])
+    bimestres_repetidos = []
     for i in range(1, 5):
         for bimestre in data:
-            if bimestre["NumeroBimestre"] != i:
+            if (bimestre["NumeroBimestre"] != i) or (bimestre["NumeroBimestre"] in bimestres_repetidos):
                 continue
+
+            bimestres_repetidos.append(bimestre["NumeroBimestre"])
 
             data_inicio = Data(bimestre["DataInicio"])
             data_fim = Data(bimestre["DataFim"])
 
-            print(f"{bimestre["NumeroBimestre"]}º Bimestre:\nInicia: {data_inicio.dia_mes()} termina: {data_fim.dia_mes()}")
+            print(f"{bimestre["NumeroBimestre"]}º Bimestre: {data_inicio.dia_mes()} - {data_fim.dia_mes()}")
